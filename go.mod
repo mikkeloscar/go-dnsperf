f42c7266330c04b69aa02812cbeac39224a814cf
Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>
2021-02-08 11:41:34 +0100

    Add prometheus metrics
    Signed-off-by: Mikkel Oscar Lyderik Larsen <mikkel.larsen@zalando.de>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mikkeloscar/go-dnsperf
 
 go 1.15
+
+require github.com/prometheus/client_golang v1.9.0
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 	"os"
 	"os/signal"
 	"sort"
@@ -13,29 +14,61 @@ import (
 	"sync"
 	"syscall"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
-	rps      int
-	names    string
-	logging  bool
-	timeout  time.Duration
-	duration time.Duration
+	config struct {
+		RPS        int
+		Names      string
+		Logging    bool
+		Timeout    time.Duration
+		Duration   time.Duration
+		MetricAddr string
+	}
+
+	dnsLookupHisto = promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace: "dnsperf",
+		Subsystem: "lookup",
+		Name:      "duration_seconds",
+		Help:      "Duration for DNS lookup measured by the client",
+		Buckets:   []float64{0.001, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.25, 0.5, 1, 2, 3, 4, 5},
+	})
+	dnsLookupErrorsTotal = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Namespace: "dnsperf",
+			Subsystem: "lookup",
+			Name:      "errors_total",
+			Help:      "Number of lookup errors.",
+		},
+	)
+	dnsLookupSuccessTotal = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Namespace: "dnsperf",
+			Subsystem: "lookup",
+			Name:      "success_total",
+			Help:      "Number of successful lookups.",
+		},
+	)
 )
 
 func main() {
-	flag.IntVar(&rps, "rps", 10, "DNS lookups per second.")
-	flag.StringVar(&names, "names", "google.com", "Comma separated list of hostnames")
-	flag.BoolVar(&logging, "enable-logging", true, "Whether to enable logging or not")
-	flag.DurationVar(&timeout, "timeout", 1*time.Second, "Timeout for DNS queries")
-	flag.DurationVar(&duration, "duration", -1, "Duration of the test, defaults to forever")
+	flag.IntVar(&config.RPS, "rps", 10, "DNS lookups per second.")
+	flag.StringVar(&config.Names, "names", "google.com", "Comma separated list of hostnames")
+	flag.BoolVar(&config.Logging, "enable-logging", true, "Whether to enable logging or not")
+	flag.DurationVar(&config.Timeout, "timeout", 1*time.Second, "Timeout for DNS queries")
+	flag.DurationVar(&config.Duration, "duration", -1, "Duration of the test, defaults to forever")
+	flag.StringVar(&config.MetricAddr, "metric-addr", ":9090", "Metric address to listen on")
 	flag.Parse()
 
-	hostNames := strings.Split(names, ",")
+	hostNames := strings.Split(config.Names, ",")
 
-	log.Print(rps)
+	log.Print(config.RPS)
 
-	ticker := time.NewTicker(1 * time.Second / time.Duration(rps))
+	ticker := time.NewTicker(1 * time.Second / time.Duration(config.RPS))
 	defer ticker.Stop()
 
 	sigs := make(chan os.Signal, 1)
@@ -45,23 +78,37 @@ func main() {
 
 	dnsResolver := &CustomResolver{
 		resolver: &net.Resolver{},
-		logging:  logging,
+		logging:  config.Logging,
 		hg:       newHG(),
 	}
 
 	var done <-chan time.Time
-	if duration > 0 {
-		done = time.After(duration)
+	if config.Duration > 0 {
+		done = time.After(config.Duration)
 	}
+
+	server := setupServer(config.MetricAddr)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go handleSigterm(server, cancel)
+	go func(server *http.Server) {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Metric server failed: %v", err)
+		}
+	}(server)
 
 	for {
 		select {
 		case <-ticker.C:
-			go dnsResolver.Lookup(hostNames[i], timeout)
+			go dnsResolver.Lookup(hostNames[i], config.Timeout)
 		case <-sigs:
 			dnsResolver.hg.printStats()
 			return
 		case <-done:
+			dnsResolver.hg.printStats()
+			return
+		case <-ctx.Done():
 			dnsResolver.hg.printStats()
 			return
 		}
@@ -81,11 +128,14 @@ type CustomResolver struct {
 }
 
 func (r *CustomResolver) Lookup(name string, timeout time.Duration) {
-	ctx, _ := context.WithTimeout(context.Background(), timeout)
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	start := time.Now()
 	names, err := r.resolver.LookupHost(ctx, name)
+	cancel()
 	duration := time.Since(start)
+	dnsLookupHisto.Observe(duration.Seconds())
 	if err != nil {
+		dnsLookupErrorsTotal.Inc()
 		r.hg.record(false, duration)
 		if r.logging {
 			log.Printf("[ERROR] %s", err)
@@ -101,6 +151,7 @@ func (r *CustomResolver) Lookup(name string, timeout time.Duration) {
 		return
 	}
 
+	dnsLookupSuccessTotal.Inc()
 	r.hg.record(true, duration)
 	if r.logging {
 		log.Printf("%s - %s", name, names)
@@ -181,4 +232,27 @@ func printBuckets(title string, b map[time.Duration]int) {
 func (hg *hg) printStats() {
 	printBuckets("success", hg.success)
 	printBuckets("failure", hg.failure)
+}
+
+// handleSigterm handles SIGTERM signal sent to the process.
+func handleSigterm(server *http.Server, cancelFunc func()) {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGTERM)
+	<-signals
+	log.Println("Received Term signal. Terminating...")
+	server.Close()
+	cancelFunc()
+}
+
+func setupServer(listenAddr string) *http.Server {
+	handler := http.NewServeMux()
+	handler.Handle("/metrics", promhttp.Handler())
+	handler.Handle("/healthz", http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	return &http.Server{
+		Addr:    listenAddr,
+		Handler: handler,
+	}
 }
